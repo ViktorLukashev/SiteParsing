d09c87ad62c9126d940dfcdb8ca3670c10916f85
ViktorLukashev <viktorlukashev@yandex.ru>
2023-07-22 15:20:34 +0300

    добавил код для записи файла в Exel, но открывать ексел нельзя

---
+++ src/main/java/com/abbott/ExcelWriter.java
@@ -0,0 +1,28 @@
+package com.abbott;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ExcelWriter {
+    public void writeDataToExcel(String data) {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Data");
+
+        String[] lines = data.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            Row row = sheet.createRow(i);
+            Cell cell = row.createCell(0);
+            cell.setCellValue(lines[i]);
+        }
+
+        try (FileOutputStream outputStream = new FileOutputStream("src/main/resources/getText.xlsx")) {
+            workbook.write(outputStream);
+            System.out.println("Данные записаны в Excel файл.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/abbott/Main.java
+++ src/main/java/com/abbott/Main.java
@@ -1,9 +1,14 @@
 package com.abbott;
 
+
 public class Main {
     public static void main(String[] args) {
         GetDataFromWebsite getDataSite = new GetDataFromWebsite();
         UpdateTimer timer = new UpdateTimer();
-        timer.startFetchingData(getDataSite.searchElement());
+        String data = getDataSite.searchElement();
+        timer.startFetchingData(data);
+
+        ExcelWriter excelWriter = new ExcelWriter();
+        excelWriter.writeDataToExcel(data);
     }
 }
